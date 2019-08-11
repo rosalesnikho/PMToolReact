974033c572917db55e8c9f92c62ef5163f506fc8
nikvicros <nikho.rosales@gmail.com>
2019-08-11 09:04:09 -0500

    added more comps

---
+++ src/components/Project/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ProjectItem extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Project Item</h1>
+			</div>
+		);
+	}
+}
+
+export default ProjectItem;
